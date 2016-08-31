1093790dc7e0534e859f93528c0b71dbcd4be062
Nathan Cantelmo <n.cantelmo@gmail.com>
2016-08-31 10:07:36 -0400

    Now with more addition

--- src/test.c
+++ src/test.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+int add(int, int);
+
 int main() {
   printf("Hello, world!\n");
+
+  int added = add(3, 5);
+  printf("add(3,5) result: %d\n", added);
+
   return 0;
+}
+
+int add(int x, int y) {
+  return x + y;
 }
 
